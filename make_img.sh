35f8d8eb1b999f48302f9641b223f07258aed532
coderaiser <mnemonic.enemy@gmail.com>
2013-04-12 17:35:47 +0300

    Update make_img.sh

--- make_img.sh
+++ make_img.sh
@@ -1,6 +1,8 @@
 #download fasm and complie nemizida sources
 wget http://flatassembler.net/fasm-1.70.03.tgz
+wget http://prdownloads.sourceforge.net/dosemu/dosemu-1.4.0-bin.tgz?download
 mv fasm*tgz fasm.tar.gz
+tar zxf dosemu*tgz
 tar -zxf fasm.tar.gz
 fasm/fasm fat12boot.asm
 fasm/fasm kernel/kernel.asm
