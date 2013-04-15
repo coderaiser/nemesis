97e69117c74e4bf5d3b7c4c3c2c338e6053eb97c
coderaiser <mnemonic.enemy@gmail.com>
2013-04-15 15:52:16 +0300

    Update and rename make_img.sh to make.sh

---
+++ make.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+FILE_EXIST=`ls -al | grep fasm`
+if [ "$FILE_EXIST" == "" ]
+  then
+    wget http://flatassembler.net/fasm-1.70.03.tgz
+    mv fasm*tgz fasm.tar.gz
+    tar -zxf fasm.tar.gz
+fi
+
+fasm/fasm fat12boot.asm
+fasm/fasm kernel/kernel.asm
+fasm/fasm shell/shell.asm
