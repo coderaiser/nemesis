ba367433100935956097cea9b3c89ec2cd15cb6a
coderaiser <mnemonic.enemy@gmail.com>
2013-04-23 09:27:04 -0400

    minor changes

--- scripts/bootwrite.js
+++ scripts/bootwrite.js
@@ -3,25 +3,33 @@
     
     var fs          = require('fs'),
         
+        BOOT_FILE   = 'fat12boot.bin',
+        IMAGE_FILE  = 'nemizida.img',
+        
+        SUCCESS_MSG = 'Boot Record writed successfully.',
+        
         OFFSET      = 0,
         POSITION    = 0;
     
     
-    fs.readFile('fat12boot.bin', function(pError, pData){
-        if(pError)
-            console.log(pError);
-        else
-            fs.open('nemizida.img', 'r+', function(pError, pFD){
-                if(pError)
-                    console.log(pError);
-                else
-                    fs.write(pFD, pData, OFFSET, pData.length, POSITION, function(pError){
-                        if(pError)
-                            console.log(pError);
-                        else
-                            console.log('Boot Record writed successfully');
-                    });
+    fs.readFile(BOOT_FILE, function(pError, pData){
+        if( !show(pError) )
+            fs.open(IMAGE_FILE, 'r+', function(pError, pFD){
+                show(pError) || write(pFD, pData);
             });
     });
+    
+    function write(pFD, pData){
+        fs.write(pFD, pData, OFFSET, pData.length, POSITION, function(pError){
+            show(pError || SUCCESS_MSG);
+        });
+    }
+    
+    function show(pMsg){
+        if(pMsg)
+            console.log(pMsg);
+        
+        return pMsg;
+    }
     
 })();
